4f8b9783c74d5853d2fb8a51baf61a91ab8eb99c
Semen Gzhesiak <brutalsam@gmail.com>
2015-12-24 21:52:27 +0200

    39 th base

---
+++ 39/kotBase.py
@@ -0,0 +1,18 @@
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice  
+import time
+device = MonkeyRunner.waitForConnection()  
+device.touch(10, 10, "DOWN_AND_UP")
+time.sleep(0.42)
+device.touch(10, 10, "DOWN_AND_UP")
+time.sleep(0.13)
+device.touch(10, 10, "DOWN_AND_UP")
+time.sleep(1.2)
+device.touch(10, 10, "down_and_up")
+time.sleep(0.55)
+device.touch(10, 10, "down_and_up")
+time.sleep(1.45)
+device.touch(10, 10, "down_and_up")
+time.sleep(0.55)
+device.touch(10, 10, "down_and_up")
+time.sleep(0.55)
+device.touch(10, 10, "down_and_up")
