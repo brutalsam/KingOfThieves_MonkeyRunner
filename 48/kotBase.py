eb1407da673de9334b43135a8f99152e7c661189
Semen Gzhesiak <Semen_Gzhesiak@epam.com>
2015-12-30 13:53:21 +0200

    Added another configuration of 48s base

--- 48/kotBase.py
+++ 48/kotBase.py
@@ -1,9 +1,9 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice  
 import time
-time.sleep(1)
+time.sleep(0.1)
 device = MonkeyRunner.waitForConnection()  
 device.touch(10, 10, "DOWN_AND_UP")
-time.sleep(0.086)
+time.sleep(0.088)
 device.touch(10, 10, "DOWN_AND_UP")
 time.sleep(1.9)
 device.touch(10, 10, "DOWN_AND_UP")
@@ -11,9 +11,9 @@ time.sleep(0.29)
 device.touch(10, 10, "down_and_up")
 time.sleep(1.2)
 device.touch(10, 10, "DOWN_AND_UP")
-time.sleep(0.94)
+time.sleep(0.955)
 device.touch(10, 10, "down_and_up")
-time.sleep(0.717)
+time.sleep(0.76)
 device.touch(10, 10, "down_and_up")
 
 
